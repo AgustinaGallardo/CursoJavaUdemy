63fc851a68d4731c575772fec83f6fd960db5b1c
Agustina Gallardo <agusgallardo275@gmail.com>
2022-11-20 18:14:14 -0300

    Import java.util.*

---
+++ Prueba.java
@@ -0,0 +1,14 @@
+import java.util.*;//Con * te importa todo;
+//Podes pedirle que te importe solo la clase import java.util.Sacanner;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		
+		String Nombre;
+		
+		Scanner MiObjeto;
+
+	}
+
+}
