ed169e4591b6b7575bcc7483ca0058d78560c70c
Agustina Gallardo <agusgallardo275@gmail.com>
2022-11-20 17:42:01 -0300

    CadenasIII

--- Cadenas2.java
+++ Cadenas2.java
@@ -5,6 +5,13 @@ public class Cadenas2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		String frase = "Cuando tengas una duda, guiate por tu nariz";
+		
+		String fraseResumen = frase.substring(24, 43);
+		
+		System.out.println(fraseResumen);
+		
+		
 	}
 
 }
