34eed6e0ab9804201a2d51181d36d8d746ced209
Agustina Gallardo <agusgallardo275@gmail.com>
2023-01-30 10:56:05 -0300

    SobreCraga de funciones

--- Funciones.java
+++ Funciones.java
@@ -5,6 +5,8 @@ public class Funciones {
 	public static void main(String[] args) {
 		
 		holaMundo();
+		holaMundo(4);
+		holaMundo("agsutina");
 		
 		String hola = devolverHola();
 		System.out.println(hola);
@@ -21,9 +23,20 @@ public class Funciones {
 		System.out.println("HolaMundo");
 	}
 	
-	private static void holaMundo (String name) {
-		System.out.println("HolaMundo" + name);
+	private static void holaMundo(String name) {
+		System.out.println("Hola " + name);
 	}
 	
+     private static void holaMundo(Integer number) {
+		System.out.println("El numero es: " + number);
+	}
+	
+	private static void holaMundo(String name, String surname) {
+		System.out.println("Hola" + name + " "+ surname);
+	}
+	
+	private static int sum(int num1, int num2) {
+		return num1 + num2;
+	}
 
 }
